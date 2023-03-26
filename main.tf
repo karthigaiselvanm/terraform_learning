218e2e338280c4f42408d99df8cdd77fcfb8be31
Karthigai Selvan <karthigaiselvanm@yahoo.com>
2023-03-26 17:46:01 +0530

    Initial Terraform commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+    ami         =   "ami-00c39f71452c08778"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "terraform-example"
+    }
+}
